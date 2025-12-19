bb48cc12f781db244ed0af9b5168c01334bc16cf
Adarshswagh <waghmareadarsh1432@gmail.com>
2025-12-19 13:26:33 +0530

    universities changed

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("582ca8041ee7a146")
+/******/ 		__webpack_require__.h = () => ("28b9f5471694fb43")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f3c6db0fc556056"; }
+/******/ 		__webpack_require__.h = function() { return "2e21d9f983445c48"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
